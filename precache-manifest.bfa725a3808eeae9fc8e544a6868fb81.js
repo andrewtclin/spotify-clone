a22786acdb79774b50563be032ebefe1054b3548
Andrew <chuntcdj@gmail.com>
2020-09-11 11:55:14 +0800

    Updates

---
+++ precache-manifest.bfa725a3808eeae9fc8e544a6868fb81.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe4837aa2a636260438a594d3d3051d3",
+    "url": "/spotify-clone/index.html"
+  },
+  {
+    "revision": "3cb8aebb44f6f82d6e19",
+    "url": "/spotify-clone/static/css/main.bc3cd6d3.chunk.css"
+  },
+  {
+    "revision": "045d769e265212d51c2b",
+    "url": "/spotify-clone/static/js/2.1e329ea7.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/spotify-clone/static/js/2.1e329ea7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3cb8aebb44f6f82d6e19",
+    "url": "/spotify-clone/static/js/main.a9a82112.chunk.js"
+  },
+  {
+    "revision": "179e35a3ac9d012bbc27",
+    "url": "/spotify-clone/static/js/runtime-main.67045fb5.js"
+  }
+]);
